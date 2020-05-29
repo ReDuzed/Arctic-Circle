9d5ac382d459f5c4c0e37847bc240e70007c6e39
ReDuzed <46213546+ReDuzed@users.noreply.github.com>
2020-05-29 14:46:21 -0700

    v0.1
    Requires the files that are not included in this upload which are found in the .csproj.

---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using TShockAPI;
+using TerrariaApi.Server;
+using RUDD.Dotnet;
+
+using ItemClasses;
+using TeamSetQueue;
+
+namespace ArcticCircle
+{
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Author => "";
+        public override string Name => "";
+        public override string Description => "";
+        public override Version Version => new Version(1, 0, 0, 0);
+        public Plugin(Main game) : base (game)
+        {
+
+        }
+        public ItemClassesV2 Classes;
+        public TeamSetV2 TeamSet; 
+        public override void Initialize()
+        {
+            Classes = new ItemClassesV2(this);
+            TeamSet = new TeamSetV2(this);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {   
+                Classes.Dispose(disposing, this);
+                TeamSet.Dispose(disposing, this);
+            }
+        }
+    }
+}
